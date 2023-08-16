9b8979b0cf90fbf61cefcc265f07019e625ade5d
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-08-16 09:11:25 +0300

    Write toString function

--- linked-list.js
+++ linked-list.js
@@ -20,7 +20,34 @@ const linkedListFactory = () => {
         let tailNode = tail();
         let newNode = nodeFactory();
         newNode.value = value;
-        tailNode.newNode = newNode;
+        if (tailNode) tailNode.nextNode = newNode;
+        else head = newNode;
     }
 
+    const toString = () => {
+        let current = head;
+        let s = ''
+        while (true) {
+            if (current) {
+                s += `( ${current.value} ) -> `;
+                current = current.nextNode;
+            }
+            else {
+                s += `( ${current} )`;
+                break;
+            }
+        }
+        console.log(s)
+    }
+
+    return {tail, append, toString}
 }
+
+let myList = linkedListFactory();
+
+myList.append(4);
+myList.append(42);
+myList.append(2);
+myList.append(6);
+myList.append(6);
+myList.toString();
